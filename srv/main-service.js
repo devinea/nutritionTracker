9e4912da2d32d15b9ae9206e3037ab45f2009727
Thorsten Müller <thorsten.mueller@js-soft.com>
2021-06-17 09:27:15 +0200

    feat: create Meal without nutrition values

--- srv/main-service.js
+++ srv/main-service.js
@@ -1,54 +1,36 @@
-// module.exports = async (srv) => {
-//     srv.on('Meal', async (req) => {
-//         console.log('hello there')
-        
-
-//     })
-//     this.before ('READ',`*`, (req)=>{console.log('hello there')})
-// }
-
 const cds = require('@sap/cds')
 module.exports = function (){
-//   this.after ('READ',`Meals`, (req, res)=>{
-//       let aIngredients = req[0].ingredient
-//       let energy = 0
-//       let proteins = 0
-//       let carbs = 0
-//       let fats = 0
-//       let fibers = 0
-//       aIngredients.forEach(element => {
-//           energy += element.energy
-//           proteins += element.proteins
-//           carbs += element.carbs
-//           fats += element.fats
-//           fibers += element.fibers
-//       })
-//       res.results[0].energy = energy
-//       res.results[0].proteins = proteins
-//       res.results[0].carbs = carbs
-//       res.results[0].fats = fats
-//       res.results[0].fibers = fibers
-//     })
-    //TODO create a Meal with value 0 for the nutrition values
 
+    this.before('CREATE', 'Meals', async (req) => {
+        //update nutrition values if not provided or valued as null
+        if (! req.data.carbs || req.data.carbs == null)
+            req.data.carbs = 0
+        if (! req.data.fats || req.data.fats == null)
+            req.data.fats = 0
+        if (! req.data.proteins || req.data.proteins == null)
+            req.data.proteins = 0
+        if (! req.data.fibers || req.data.fibers == null)
+            req.data.fibers = 0
+        if (! req.data.energy || req.data.energy == null)
+            req.data.energy = 0
+
+        //update date
+        if (! req.data.dateEaten || req.data.dateEaten == null)
+            req.data.dateEaten = new Date().toISOString()
+    })
     
     this.after ('CREATE', 'Ingredients', async (req, res) => {
         const { Meals } = cds.entities
-        let query = SELECT.from(Meals).where({ID:req.meal[0].meal_ID})
+        let mealID = req.meal[0].meal_ID
+        let query = SELECT.from(Meals).where({ID:mealID})
         let meal = await cds.run (query)
 
-        meal.energy += req.energy
-        meal.proteins += req.proteins
-        meal.carbs += req.carbs
-        meal.fats += req.fats
-        meal.fibers += req.fibers
-
         //update database entry
-        await UPDATE (Meals) .where('ID =', req.meal[0].meal_ID).set (`energy +=`, req.energy) 
-        await UPDATE (Meals) .where('ID =', req.meal[0].meal_ID).set (`proteins +=`, req.proteins) 
-        await UPDATE (Meals) .where('ID =', req.meal[0].meal_ID).set (`carbs +=`, req.carbs) 
-        await UPDATE (Meals) .where('ID =', req.meal[0].meal_ID).set (`fats +=`, req.fats) 
-        await UPDATE (Meals) .where('ID =', req.meal[0].meal_ID).set (`fibers +=`, req.fibers) 
+        await UPDATE (Meals) .where('ID =', mealID).set (`energy +=`, req.energy) 
+        await UPDATE (Meals) .where('ID =', mealID).set (`proteins +=`, req.proteins) 
+        await UPDATE (Meals) .where('ID =', mealID).set (`carbs +=`, req.carbs) 
+        await UPDATE (Meals) .where('ID =', mealID).set (`fats +=`, req.fats) 
+        await UPDATE (Meals) .where('ID =', mealID).set (`fibers +=`, req.fibers) 
 
         
     })
