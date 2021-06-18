c1d98272d69661cb8ab432a3b72f5caaceb1637a
Thorsten Müller <thorsten.mueller@js-soft.com>
2021-06-18 08:53:41 +0200

    feat: add ui5 app

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["tm/nutriTracker/myUI5App/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("tm.nutriTracker.myUI5App.controller.MainView", {});
+});
