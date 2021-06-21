f437f8514dc9086d98e4f71bf5c8e36f55c70e5f
Thorsten Müller <thorsten.mueller@js-soft.com>
2021-06-21 15:13:16 +0200

    feat: enable master detail layout

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "tm/nutriTracker/myUI5App/controller/BaseController"
+  ], function(Controller) {
+    "use strict";
+  
+    return Controller.extend("tm.nutriTracker.myUI5App.controller.App", {
+        onInit(){
+        }
+    });
+    });  
