3627ffc0873e7e48981294a3d979b85031c81db6
Thorsten Müller <thorsten.mueller@js-soft.com>
2021-06-22 08:30:28 +0200

    feat: automatically nav to detail view when creating a new meal

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -9,6 +9,16 @@ sap.ui.define(["tm/nutriTracker/myUI5App/controller/BaseController"], function (
         displayNutriDetails (oEvent) {
             this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded")
             this.getRouter().navTo('NutriDetailView', {ID: oEvent.getSource().getBindingContext().getObject().ID})
+        },
+
+        addMeal(){
+            let oContext = this.getView().byId('mealsList').getBinding('items').create({})
+            oContext.created().then(
+                () => {
+                    this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded")
+                    this.getRouter().navTo('NutriDetailView', {ID: oContext.getObject().ID})
+                }
+            )
         }
     });
 });
